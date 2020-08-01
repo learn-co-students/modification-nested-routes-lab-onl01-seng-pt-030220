e6184435fbd9500c2052ffb240b441879dbd2f33
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-31 22:39:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# Rails.application.routes.draw do
+#   resources :artists do
+#     resources :songs, only: [:index, :show, :new, :edit, :create]
+#     #artists/artist_id/songs
+#     #artist_songs_path
+#   end
+#   resources :songs
+# end
+
 Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show, :new, :edit]
